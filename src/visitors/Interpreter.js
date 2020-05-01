e586d27b45b7a6040034733fa83d45d66a09313c
mj3wc <wcronk@smu.edu>
2020-05-01 15:48:27 -0500

    copied pragdave week3 start code from canvas

---
+++ src/visitors/Interpreter.js
@@ -0,0 +1,100 @@
+function bool(value) {
+  return value ? 1 : 0
+}
+
+const operations = {
+  "+": (l, r) => l + r,
+  "-": (l, r) => l - r,
+  "*": (l, r) => l * r,
+  "/": (l, r) => Math.round(l / r),
+
+  "<":  (l,r) => bool(l < r),
+  "<=": (l,r) => bool(l <= r),
+  "==": (l,r) => bool(l == r),
+  ">=": (l,r) => bool(l >= r),
+  ">":  (l,r) => bool(l > r),
+  "!=": (l,r) => bool(l != r),
+
+}
+
+import Binding from "../binding.js"
+
+export default class Interpreter {
+
+  constructor(target, printFunction) {
+    this.target = target
+    this.printFunction = printFunction
+    this.binding = new Binding()
+  }
+
+  visit() {
+    return this.target.accept(this)
+  }
+
+  Assignment(node) {
+    let variable = node.variable.accept(this)
+    let expr     = node.expr.accept(this)
+    this.binding.updateVariable(variable, expr)
+    return expr
+  }
+
+  BinOp(node) {
+    let l = node.l.accept(this)
+    let r = node.r.accept(this)
+    return operations[node.op](l, r)
+  }
+
+  FunctionCall(node) {
+    let bodyAst = node.name.accept(this)
+    return bodyAst.accept(this)
+  }
+
+  FunctionDefinition(node) {
+    return node.code
+  }
+
+  IfStatement(node) {
+    let predicate = bool(node.predicate.accept(this))
+
+    if (predicate == 1)
+      return node.thenCode.accept(this)
+
+    return node.elseCode.accept(this)
+  }
+
+  IntegerValue(node) {
+    return node.value
+  }
+
+  InternalPrint(node) {
+    let args = node.args.map(a => a.accept(this).toString() )
+    this.printFunction(args)
+    return args
+  }
+
+  StatementList(node) {
+    let result = 0
+    node.statements.forEach(statement =>
+      result = statement.accept(this)
+    )
+    return result
+  }
+
+  VariableDeclaration(node) {
+    let variable = node.variable.accept(this)
+    let initialValue = 0
+    if (node.initialValue) {
+      initialValue = node.initialValue.accept(this)
+    }
+    this.binding.setVariable(variable, initialValue)
+    return initialValue
+  }
+
+  VariableName(node) {
+    return node.name
+  }
+
+  VariableValue(node) {
+    return this.binding.getVariableValue(node.name)
+  }
+}
